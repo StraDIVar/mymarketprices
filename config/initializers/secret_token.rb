a9e7345ee96d2a5013549dedafdbb009f3ab8da2
Dmitry Stratsevsky <dmitry.stratsevsky@warecorp.com>
2013-11-30 01:25:40 +0300

    init rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mymarketprices::Application.config.secret_token = '946ba7135de20a2d13f63ecd8d474954472ab2e2ca8e1439448483a28e65ac5665b87ad2c80da78c8de4fbb8dc7508a6afb0633d812e16024111233fc133814b'
