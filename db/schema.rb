bef573478d692444d574d6db583c76eca17471fa
Dmitry Stratsevsky <dmitry.stratsevsky@warecorp.com>
2013-11-30 23:26:53 +0300

    add username attribute to user; authorization will use username instead email

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131130182520) do
+ActiveRecord::Schema.define(:version => 20131130201321) do
 
   create_table "goods", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(:version => 20131130182520) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
